aabbdbd470ef9d6b52d408397b3cf535bf6c8ab5
Konstantinos Ntellas <76548087+konntl@users.noreply.github.com>
2023-03-02 21:18:35 +0200

    Add files via upload

---
+++ lab1-1.py
@@ -0,0 +1,33 @@
+import requests  # εισαγωγή της βιβλιοθήκης
+
+def more(text):
+    count = 0
+    for line in text.split('\n'):
+        print(line)
+        count += 1
+        if count % 30 == 0:
+            reply = input('Show more (y/n)? ')
+            if reply == 'n':
+                break
+
+def webserversoftware(k,v):
+    if k == 'Server':
+        print("Web Server Software : ",v)
+    else : pass
+
+url = input("enter URL : ")
+
+with requests.get(url) as response:
+    for k,v in response.headers.items():
+        print("{} : {}".format(k,v))
+    print('\n')
+    for k,v in response.headers.items():
+        webserversoftware(k,v)
+    print(response.cookies)
+    for k,v in enumerate(response.cookies):
+        print('Cookies : ')
+        print('{} : {}'.format(v.name,v.expires))
+
+    
+    
+    
